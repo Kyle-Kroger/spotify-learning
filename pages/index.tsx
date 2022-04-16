f5296a33ee3066175da7ff73e9845cdf9586aead
Kyle Kroger <kyle.s.kroger@gmail.com>
2022-04-16 15:13:55 -0500

    add index header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/gradientLayout";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>Homepage is coming from index.tsx</div>;
+  return (
+    <GradientLayout
+      color="blue"
+      subtitle="profile"
+      title="Kyle Kroger"
+      description="10 Playlists"
+      roundImage
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
