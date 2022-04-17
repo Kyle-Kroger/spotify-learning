2a67564a1e26c33c563678e68f923f1af6ae9132
Kyle Kroger <kyle.s.kroger@gmail.com>
2022-04-16 20:59:49 -0500

    add song table

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,9 +1,37 @@
+import GradientLayout from "../../components/gradientLayout";
+import SongTable from "../../components/songsTable";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  console.log(playlist);
-  return <div>a playlist</div>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+    >
+      <SongTable songs={playlist.songs} />
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
